fb1b02b56ed54b299ec7325ec1a628c9edb15f01
Akhilesh Allewar <akhileshallewar@Akhileshs-MacBook-Air.local>
2023-10-06 20:28:53 +0530

    header ui component

--- apps/food-delivery-app/src/app/app.module.ts
+++ apps/food-delivery-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonUiModule } from '@food-delivery-app/common-ui';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
+    CommonUiModule,
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
---
+++ libs/shared/ui/src/lib/header/logo/logo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'food-delivery-app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.css'],
+})
+export class LogoComponent {}
