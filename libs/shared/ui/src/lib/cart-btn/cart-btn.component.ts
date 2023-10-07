c3b48151b49c5170bdf7947fd04e57eeea7aa3a1
Akhilesh Allewar <akhileshallewar@Akhileshs-MacBook-Air.local>
2023-10-07 21:51:23 +0530

    working on restaurant card

---
+++ libs/shared/ui/src/lib/cart-btn/cart-btn.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'food-delivery-app-cart-btn',
+  templateUrl: './cart-btn.component.html',
+  styleUrls: ['./cart-btn.component.css'],
+})
+export class CartBtnComponent {}
--- libs/shared/ui/src/lib/common-ui.module.ts
+++ libs/shared/ui/src/lib/common-ui.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LogoComponent } from './header/logo/logo.component';
+import { CartBtnComponent } from './cart-btn/cart-btn.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HeaderComponent, FooterComponent, LogoComponent],
-  exports: [HeaderComponent,LogoComponent, FooterComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CartBtnComponent,
+  ],
+  exports: [HeaderComponent, LogoComponent, FooterComponent, CartBtnComponent],
 })
 export class CommonUiModule {}
